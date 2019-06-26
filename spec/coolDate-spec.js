cb9cca89307a7f4c7e9e4cb4c757882df060c34b
Rochelle Roberts <rochelle@>
2019-06-26 15:06:59 -0700

    finally works

--- spec/coolDate-spec.js
+++ spec/coolDate-spec.js
@@ -6,10 +6,12 @@ import { CoolDate } from './../src/coolDate.js';
 describe('CoolDate', function() {
 
   it('should test whether constructor works', function() {
-    var mydate = new CoolDate(2016, 5, 24);
+    let mydate = new CoolDate(2016, 5, 24);
     expect(mydate.getFullYear()).toEqual(2016);
     expect(mydate.getMonth()).toEqual(5);
     expect(mydate.getDate()).toEqual(24);
   });
+
+
 
 });
--- src/coolDate.js
+++ src/coolDate.js
@@ -1,10 +1,31 @@
 export class CoolDate extends Date{
-  constructor(){
+  constructor(year, month, day){
     super();
+    this.setFullYear(year);
+    this.setMonth(month);
+    this.setDate(day);
   }
 
-  getWeekday(num){
+  getWeekday(){
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    return days[num];
+    // w = (d+ [2.6m - .2] + y + [y/4] + [c/4] - 2c)mod7
+    let num = days[this.getDay()];
+
+
+    return num;
   }
 }
+
+
+
+
+
+
+// Y is the year minus 1 for January or February, and the year for any other month
+// y is the last 2 digits of Y
+// c is the first 2 digits of Y
+// d is the day of the month (1 to 31)
+// m is the shifted month (March=1,...,February=12)
+// w is the day of week (0=Sunday,...,6=Saturday). If w is negative you have to add 7 to it.
+
+// let monthDictionary = ["no", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December", "January", "February"]
